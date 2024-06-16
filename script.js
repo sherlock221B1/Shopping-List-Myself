b50857380e457a1bbe227a95349f0452be25c8c1
STK <nordvpn4466@gmail.com>
2024-06-16 15:09:27 +0630

    SP list ongoing

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ let count = 0;
 inputTag.addEventListener("change", (event) => {
   count += 1;
 
+  const productTag = document.createElement("div");  //create productTag
+  productTag.classList.add("productTag"); // add class list (productTag)
+  const product = `${count}. ${event.target.value}`; // make product
+  productTag.append(product); // append product to productTag
+
+  //const binContainerTag = document.createElement("div");// create binContainerTag
+  // <i class="fa-solid fa-trash-can"></i>
+  const binTag = document.createElement("i");
+  binTag.classList.add("fa-solid","fa-trash-can");
+  //binContainerTag.append(binTag); // append binTag to binContaierTag
+
   const listTag = document.createElement("div");
-  listTag.classList.add("listTag");
-  const product = `${count}. ${event.target.value}`;
-  listTag.append(product);
-  listContainerTag.append(listTag);
+  listTag.classList.add("listTag") // add class list (listTag)
+  listTag.append(productTag,binTag); // append productTag and binContainerTag to listTag
+
+  listContainerTag.append(listTag); // append listTag to listContainerTag
 
   isDone = false;
   listTag.addEventListener("click", () => {
     if (!isDone) {
-      listTag.classList.add("doneList");
+      productTag.classList.add("doneList");
       isDone = true;
     } else {
-      listTag.classList.remove("doneList");
+      productTag.classList.remove("doneList");
       isDone = false;
     }
   });
